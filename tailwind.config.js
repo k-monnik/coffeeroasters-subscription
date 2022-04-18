42ba11bfbc6bc10f4da3c7036a82b88ea73a3e12
Kristen Monnik <nolaviolinist@gmail.com>
2022-04-18 14:49:04 -0400

    add custom fonts and color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'dark-cyan': '#0e8784',
+      'dark-grey-blue': '#333d4b',
+      'pale-orange': '#fdd6ba',
+      'cream': '#fefcf7',
+      'grey': '#83888f',
+    },
+    extend: {
+      fontFamily: {
+        Fraunces: ['Fraunces', 'serif'],
+        Barlow: ['Barlow', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
